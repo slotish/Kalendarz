41e362d0094fcf9ad10f23decc074a5cfbd872d6
slotish <slotish@.gmail.com>
2016-08-21 19:13:59 +0200

    finishing the menu page

--- menu.js
+++ menu.js
@@ -1,5 +1,15 @@
 
 
+$( document ).ready(function() {
+    $('.carousel').carousel();
+    smoothScroll(1000);
+});
 
-    	
-		
+function smoothScroll(speed) {
+    $('#back_to_top').click(function() {
+        $('html, body').animate({
+            scrollTop:$('#top_Anchor').offset().top
+        }, speed);
+        event.preventDefault();
+    });
+}
